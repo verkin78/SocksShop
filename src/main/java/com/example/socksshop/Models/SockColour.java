6fdf18337ec54c834c3701fa37c372cb76286061
Verkin78 <otrajenie.jc@gmail.com>
2023-01-19 20:31:35 +0300

    SocksShop - Models

--- src/main/java/com/example/socksshop/Models/SockColour.java
+++ src/main/java/com/example/socksshop/Models/SockColour.java
@@ -6,5 +6,22 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 public enum SockColour {
+    BLACK("чёрный"),
+    BLUE("синий"),
+    BROWN("коричневый"),
+    GREEN("зелёный"),
+    GRAY("серый"),
+    RAD("красный"),
+    WHITE("белый"),
+    YELLOW("жёлтый");
 
+    private String colour;
+
+    SockColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return colour;
+    }
 }
