bf35690cf58365bafd0650db7a67d7e1667d73e6
Verkin78 <otrajenie.jc@gmail.com>
2023-01-19 19:57:56 +0300

    SocksShop - Библиотеки

---
+++ src/main/java/com/example/socksshop/Models/SockColour.java
@@ -0,0 +1,10 @@
+package com.example.socksshop.Models;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+public enum SockColour {
+
+}
